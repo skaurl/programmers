511fc6c62502e74b6c33b749b95817390f7bdfec
Namhyeok <dr_lunars@naver.com>
2021-09-10 08:58:46 +0900

    practice

--- Level 1/문자열 내 p와 y의 개수.py
+++ Level 1/문자열 내 p와 y의 개수.py
@@ -1,15 +1,2 @@
 def solution(s):
-    pc = 0
-    yc = 0
-    for i in s:
-        if "p" == i or "P" == i:
-            pc+=1
-        if "y" == i or "Y" == i:
-            yc+=1
-
-    if pc == yc:
-        return True
-    elif pc ==0 and yc==0:
-        return True
-    elif pc != yc:
-        return False
+    return s.lower().count('p') == s.lower().count('y')
