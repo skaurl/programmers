163ff8eaee54ecbf0b8f438bd364a51513fb40ce
Namhyeok <dr_lunars@naver.com>
2021-09-10 08:58:34 +0900

    practice

--- Level 1/K번째수.py
+++ Level 1/K번째수.py
@@ -1,5 +1,2 @@
 def solution(array, commands):
-    answer = []
-    for i in range(len(commands)):
-        answer.append(sorted(array[commands[i][0]-1:commands[i][1]])[commands[i][2]-1])
-    return answer
+    return [sorted(array[i[0]-1:i[1]])[i[2]-1] for i in commands]
