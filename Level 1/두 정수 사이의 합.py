542f68269174f528500513d97559ec31b0512dfc
Namhyeok <dr_lunars@naver.com>
2021-09-10 08:58:40 +0900

    practice

--- Level 1/두 정수 사이의 합.py
+++ Level 1/두 정수 사이의 합.py
@@ -1,8 +1,5 @@
 def solution(a, b):
-    if a > b:
-        answer = sum(list(range(b,a+1,1)))
-    elif a < b:
-        answer = sum(list(range(a, b+1, 1)))
+    if a <= b:
+        return sum(range(a,b+1))
     else:
-        answer = 0
-    return answer
+        return sum(range(b,a+1))
