44e7eb32f9bf371aaba2540505d0e79b8d7645c1
Namhyeok <dr_lunars@naver.com>
2021-09-10 08:58:43 +0900

    practice

--- Level 1/두 개 뽑아서 더하기.py
+++ Level 1/두 개 뽑아서 더하기.py
@@ -1,8 +1,7 @@
 def solution(numbers):
     answer = []
     for i in range(len(numbers)):
-        for j in range(len(numbers)):
-            if i != j:
-                answer.append(numbers[i]+numbers[j])
-    answer = sorted(list(set(answer)))
-    return answer
+        for j in range(i+1,len(numbers)):
+            print(numbers[i],numbers[j])
+            answer.append(numbers[i]+numbers[j])
+    return sorted(list(set(answer)))
