381462ed96ded9ffa6d0737956d6ad107fe01b82
Namhyeok <dr_lunars@naver.com>
2021-09-11 09:31:29 +0900

    practice

--- Level 1/나누어 떨어지는 숫자 배열.py
+++ Level 1/나누어 떨어지는 숫자 배열.py
@@ -1,10 +1,5 @@
 def solution(arr, divisor):
-    answer = []
-    for i in arr:
-        if i%divisor==0:
-            answer.append(i)
-    answer = sorted(answer)
-    if len(answer) == 0:
+    answer = sorted([i for i in arr if i%divisor==0])
+    if answer == []:
         return [-1]
-    else:
-        return answer
+    return answer
