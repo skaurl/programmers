126ea6ab7c1d0e4449ed6e1712c5742570331757
Namhyeok <dr_lunars@naver.com>
2021-09-11 09:31:18 +0900

    practice

--- Level 1/2016년.py
+++ Level 1/2016년.py
@@ -1,8 +1,4 @@
 def solution(a, b):
-    w = ["FRI","SAT","SUN","MON","TUE","WED","THU"]
-    m = [31,29,31,30,31,30,31,31,30,31,30,31]
-    if a!=1:
-        answer = w[(sum(m[:a-1]) + b - 1)%7]
-    else:
-        answer = w[(b-1)%7]
-    return answer
+    m = [0,31,29,31,30,31,30,31,31,30,31,30,31]
+    w = ['THU','FRI','SAT','SUN','MON','TUE','WED']
+    return w[(sum(m[:a])+b)%7]
