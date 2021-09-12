ae5e1821d6fb47574f8be54fbb404fb34a94bc8e
Namhyeok <dr_lunars@naver.com>
2021-09-12 12:12:38 +0900

    practice

--- Level 1/문자열 다루기 기본.py
+++ Level 1/문자열 다루기 기본.py
@@ -1,10 +1,2 @@
 def solution(s):
-    if len(s) == 4 or len(s) == 6:
-        try:
-            s = int(s)
-            answer = True
-        except:
-            answer = False
-    else:
-        answer = False
-    return answer
+    return s.isdecimal() and len(s) == 4 or len(s) == 6
