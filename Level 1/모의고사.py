e3059f648121c7fc9741795cd8be9c2bfd2fb4f4
Namhyeok <dr_lunars@naver.com>
2021-09-12 12:12:35 +0900

    practice

--- Level 1/모의고사.py
+++ Level 1/모의고사.py
@@ -1,35 +1,17 @@
 def solution(answers):
-    sum_1 = 0
-    sum_2 = 0
-    sum_3 = 0
-
-    p_1 = [1, 2, 3, 4, 5]
-    p_2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    p_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-
+    math_1 = [1, 2, 3, 4, 5]
+    math_2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    math_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = [0,0,0]
     for i in range(len(answers)):
-        if p_1[i%5] == answers[i%len(answers)]:
-            sum_1 += 1
-
-    for i in range(len(answers)):
-        if p_2[i%8] == answers[i%len(answers)]:
-            sum_2 += 1
-
-    for i in range(len(answers)):
-        if p_3[i%10] == answers[i%len(answers)]:
-            sum_3 += 1
-
-    if sum_1 == sum_2 == sum_3:
-        return [1,2,3]
-    elif sum_1 > sum_2 and  sum_1 > sum_3:
-        return [1]
-    elif sum_2 > sum_1 and  sum_2 > sum_3:
-        return [2]
-    elif sum_3 > sum_2 and  sum_3 > sum_1:
-        return [3]
-    elif sum_1 == sum_2 and  sum_1 > sum_3:
-        return [1,2]
-    elif sum_2 > sum_1 and  sum_2 == sum_3:
-        return [2,3]
-    elif sum_3 > sum_2 and  sum_3 == sum_1:
-        return [1,3]
+        if answers[i] == math_1[i%len(math_1)]:
+            score[0] += 1
+        if answers[i] == math_2[i%len(math_2)]:
+            score[1] += 1
+        if answers[i] == math_3[i%len(math_3)]:
+            score[2] += 1
+    answer = []
+    for i in range(len(score)):
+        if max(score) == score[i]:
+            answer.append(i+1)
+    return answer
