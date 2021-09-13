8dd68d8c77aba7a3dc6bf07d9d38e9de8e5e7736
Namhyeok <dr_lunars@naver.com>
2021-09-14 08:42:17 +0900

    practice

--- Level 1/약수의 개수와 덧셈.py
+++ Level 1/약수의 개수와 덧셈.py
@@ -1,14 +1,14 @@
 def solution(left, right):
     answer = 0
-    for n in range(left,right+1):
-        cnt = 0
-        for i in range(1,int(n**0.5)+1):
-            if n % i == 0:
-                cnt += 1
-                if i**2 != n:
-                    cnt += 1
-        if cnt%2 == 0:
-            answer += n
+    for i in range(left,right+1):
+        count = 0
+        for j in range(1,int(i**0.5)+1):
+            if i%j == 0:
+                count += 1
+                if j**2 != i:
+                    count += 1
+        if count%2 == 0:
+            answer += i
         else:
-            answer -= n
+            answer -= i
     return answer
