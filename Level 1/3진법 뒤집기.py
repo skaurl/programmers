dda0c64be931d71a8bb0dbb4928a65b35a4f39b8
Namhyeok <dr_lunars@naver.com>
2021-09-17 10:40:25 +0900

    practice

--- Level 1/3진법 뒤집기.py
+++ Level 1/3진법 뒤집기.py
@@ -1,14 +1,6 @@
 def solution(n):
-    n = convert(n,3)
-    answer = 0
-    for i in range(len(n)):
-        answer += int(list(n)[len(n)-1-i])*(3**(len(n)-1-i))
-    return answer
-
-def convert(n, base):
-    T = "0123456789ABCDEF"
-    q, r = divmod(n, base)
-    if q == 0:
-        return T[r]
-    else:
-        return convert(q, base) + T[r]
+    answer = ''
+    while n > 0:
+        answer += str(n%3)
+        n //= 3
+    return int(answer,3)
