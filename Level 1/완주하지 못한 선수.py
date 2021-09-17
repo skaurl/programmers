84c6f55949910b9646f8afbcf3bcef71fea6e6ff
Namhyeok <dr_lunars@naver.com>
2021-09-17 10:40:28 +0900

    practice

--- Level 1/완주하지 못한 선수.py
+++ Level 1/완주하지 못한 선수.py
@@ -1,9 +1,6 @@
 def solution(participant, completion):
-    participant = sorted(participant)
-    completion = sorted(completion)
-    for i in range(len(participant)):
-        try:
-            if participant[i] != completion[i]:
-                return participant[i]
-        except:
-            return participant[-1]
+    participant, completion = sorted(participant), sorted(completion)
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[-1]
