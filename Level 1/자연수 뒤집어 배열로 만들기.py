0ada672c6992998f50874348b7798ea1a4a08612
Namhyeok <dr_lunars@naver.com>
2021-09-18 15:09:57 +0900

    practice

--- Level 1/자연수 뒤집어 배열로 만들기.py
+++ Level 1/자연수 뒤집어 배열로 만들기.py
@@ -1,6 +1,2 @@
 def solution(n):
-    answer = []
-    n = list(str(n))
-    for i in range(len(n)):
-        answer.append(int(n[len(n)-i-1]))
-    return answer
+    return [int(i) for i in list(str(n))][::-1]
