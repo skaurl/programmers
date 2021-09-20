8dc031c6fff649b8df3a35b8d9febfb64ebf03dd
Namhyeok <dr_lunars@naver.com>
2021-09-20 13:03:56 +0900

    practice

--- Level 1/콜라츠 추측.py
+++ Level 1/콜라츠 추측.py
@@ -1,15 +1,13 @@
+def Collatz(num):
+    if num%2 == 0:
+        return num//2
+    return 3*num+1
+
 def solution(num):
     answer = 0
-    while True:
-        if num == 1:
-            break
-        if num % 2 == 0:
-            num /= 2
-            answer += 1
-        else:
-            num = num * 3 + 1
-            answer += 1
+    while num != 1:
+        num = Collatz(num)
+        answer += 1
         if answer == 500:
-            answer = -1
-            break
+            return -1
     return answer
