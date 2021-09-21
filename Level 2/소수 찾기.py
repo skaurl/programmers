5ca8f36796ccf8dd3ae48e9941a49417d278ffb7
Namhyeok <dr_lunars@naver.com>
2021-09-22 02:11:15 +0900

    practice

--- Level 2/소수 찾기.py
+++ Level 2/소수 찾기.py
@@ -1,40 +1,20 @@
+from itertools import permutations
+
 def solution(numbers):
-    numbers = list(numbers)
-    result = []
-    total = 0
-    for i in range(1,len(numbers)+1):
-        for j in perm(numbers,i):
-            result.append(int("".join(j)))
-    result = list(set(result))
-    Prime = prime(max(result))
-    for i in result:
-        if i in Prime:
-            total += 1
-    return total
+    numbers_lst = []
+    for i in range(1, len(numbers) + 1):
+        numbers_lst.extend([int(''.join(j)) for j in permutations(numbers, i)])
+    numbers_lst = set(numbers_lst)
 
-def perm(lst,n):
-    ret = []
-    if n>len(lst):
-        return ret
-    if n==1:
-        for i in lst:
-            ret.append([i])
-    else:
-        for i in range(len(lst)):
-            temp = [i for i in lst]
-            temp.remove(lst[i])
-            for p in perm(temp,n-1):
-                ret.append([lst[i]]+p)
-    return ret
+    TF = [False, False] + [True] * (max(numbers_lst) - 1)
+    for i in range(len(TF)):
+        if TF[i] == True:
+            for j in range(i + i, len(TF), i):
+                TF[j] = False
+    prime = [i for i in range(len(TF)) if TF[i] == True]
 
-def prime(n):
-    a = [False,False] + [True]*(n-1)
-    prime = []
-    for i in range(2,n+1):
-        if a[i]:
-            prime.append(i)
-            for j in range(2*i,n+1,i):
-                a[j] = False
-    return prime
-
-print(solution("011"))
+    answer = 0
+    for i in numbers_lst:
+        if i in prime:
+            answer += 1
+    return answer
