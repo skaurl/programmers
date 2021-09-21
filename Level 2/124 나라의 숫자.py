d88bfdde07be3be4d2a7af43050bf7ac0d81c70f
Namhyeok <dr_lunars@naver.com>
2021-09-22 02:11:10 +0900

    practice

--- Level 2/124 나라의 숫자.py
+++ Level 2/124 나라의 숫자.py
@@ -1,6 +1,7 @@
 def solution(n):
-    if n<=3:
-        return "124"[n-1]
-    else:
-        q, r = divmod(n-1, 3)
-        return solution(q) + "124"[r]
+    answer = ''
+    while n > 0:
+        q,r = divmod(n-1,3)
+        answer += '124'[r]
+        n = q
+    return answer[::-1]
