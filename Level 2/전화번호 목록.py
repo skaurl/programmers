10afbd7b8beb58f5faca8cf95e7cb58a66e0eadf
Namhyeok <dr_lunars@naver.com>
2021-09-21 11:48:56 +0900

    practice

--- Level 2/전화번호 목록.py
+++ Level 2/전화번호 목록.py
@@ -1,14 +1,6 @@
 def solution(phone_book):
     phone_book = sorted(phone_book)
-    answer = True
-    i = 1
-    while True:
-        try:
-            if phone_book[0] in phone_book[i][:len(phone_book[0])]:
-                answer = False
-                break
-            i+=1
-            del phone_book[0]
-        except:
-            break
-    return answer
+    for i in range(len(phone_book)-1):
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+            return False
+    return True
