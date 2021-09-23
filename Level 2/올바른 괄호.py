013b23c705085981432dbcb24002b5678f5ed037
Namhyeok <dr_lunars@naver.com>
2021-09-23 13:23:50 +0900

    practice

--- Level 2/올바른 괄호.py
+++ Level 2/올바른 괄호.py
@@ -1,14 +1,13 @@
 def solution(s):
-    s = list(s)
     stack = []
-    for i in range(len(s)):
-        if len(stack)==0:
-            stack.append(s[i])
-        elif s[i] == "(":
-            stack.append(s[i])
-        elif stack[-1] == "(" and s[i] == ")":
+    for i in s:
+        if len(stack) == 0:
+            stack.append(i)
+        elif stack[-1]+i == '()':
             stack.pop()
-    if len(stack)==0:
+        else:
+            stack.append(i)
+    if len(stack) == 0:
         return True
     else:
         return False
