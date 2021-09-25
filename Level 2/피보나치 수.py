f2737f8424a4d65aa1e585530d10b2eab42ead2a
Namhyeok <dr_lunars@naver.com>
2021-09-25 13:56:57 +0900

    practice

--- Level 2/피보나치 수.py
+++ Level 2/피보나치 수.py
@@ -1,12 +1,5 @@
 def solution(n):
-    answer = []
-    for i in range(n+1):
-        if i == 0:
-            answer.append(0)
-        elif i == 1:
-            answer.append(1)
-        elif i == 2:
-            answer.append(1)
-        else:
-            answer.append((answer[i-1]+answer[i-2])%1234567)
-    return answer[-1]
+    answer = [0,1]
+    for i in range(1,n):
+        answer.append((answer[i-1]+answer[i])%1234567)
+    return answer[n]
