7a870f50c82c8b6af0e548534937cb7f30e92ad3
Namhyeok <dr_lunars@naver.com>
2021-09-25 13:57:04 +0900

    practice

--- Level 2/행렬의 곱셈.py
+++ Level 2/행렬의 곱셈.py
@@ -1,14 +1,13 @@
 def solution(arr1, arr2):
-    arr3 = [[0] * len(arr2) for i in range(len(arr2[0]))]
-
     answer = [[0] * len(arr2[0]) for i in range(len(arr1))]
+    arr2_t = [[0] * len(arr2) for i in range(len(arr2[0]))]
 
     for i in range(len(arr2)):
         for j in range(len(arr2[i])):
-            arr3[j][i] = arr2[i][j]
+            arr2_t[j][i] = arr2[i][j]
 
     for i in range(len(answer)):
         for j in range(len(answer[i])):
-            answer[i][j] = sum([arr1[i][k]*arr3[j][k] for k in range(len(arr1[i]))])
+            answer[i][j] = sum([arr1[i][k] * arr2_t[j][k] for k in range(len(arr1[i]))])
 
     return answer
