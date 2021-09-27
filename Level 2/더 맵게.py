329ae7c9cd09543025ce66e0ff3967de5a76253c
Namhyeok <dr_lunars@naver.com>
2021-09-27 09:44:09 +0900

    practice

--- Level 2/더 맵게.py
+++ Level 2/더 맵게.py
@@ -1,18 +1,11 @@
 import heapq
 
-def mixDishes(array, k):
+def solution(scoville, K):
     answer = 0
-    while 1:
-        try:
-            newDishValue = heapq.heappop(array) + heapq.heappop(array)*2
-            heapq.heappush(array, newDishValue)
-            answer += 1
-            if array[0] >= k:
-                return answer
-        except:
+    heapq.heapify(scoville)
+    while scoville[0] < K:
+        heapq.heappush(scoville, heapq.heappop(scoville) + 2*heapq.heappop(scoville))
+        answer += 1
+        if len(scoville) == 1 and scoville[0] < K:
             return -1
-
-def solution(array, k):
-    heapq.heapify(array)
-    answer = mixDishes(array, k)
     return answer
