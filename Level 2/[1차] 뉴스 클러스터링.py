d6eabac6fe5140d390031c75da7073fa72a0fc9f
Namhyeok <dr_lunars@naver.com>
2021-10-03 13:19:35 +0900

    practice

--- Level 2/[1차] 뉴스 클러스터링.py
+++ Level 2/[1차] 뉴스 클러스터링.py
@@ -1,17 +1,9 @@
-import re
-import math
+from collections import Counter
 
 def solution(str1, str2):
-    str1 = [str1[i:i+2].lower() for i in range(0, len(str1)-1) if not re.findall('[^a-zA-Z]+', str1[i:i+2])]
-    str2 = [str2[i:i+2].lower() for i in range(0, len(str2)-1) if not re.findall('[^a-zA-Z]+', str2[i:i+2])]
-
-    gyo = set(str1) & set(str2)
-    hap = set(str1) | set(str2)
-
-    if len(hap) == 0:
+    STR1 = Counter([(str1[i]+str1[i+1]).lower() for i in range(len(str1)-1) if (str1[i]+str1[i+1]).isalpha()])
+    STR2 = Counter([(str2[i]+str2[i+1]).lower() for i in range(len(str2)-1) if (str2[i]+str2[i+1]).isalpha()])
+    try:
+        return int(len(list((STR1 & STR2).elements())) / len(list((STR1 | STR2).elements())) * 65536)
+    except:
         return 65536
-
-    gyo_sum = sum([min(str1.count(gg), str2.count(gg)) for gg in gyo])
-    hap_sum = sum([max(str1.count(hh), str2.count(hh)) for hh in hap])
-
-    return math.floor((gyo_sum/hap_sum)*65536)
