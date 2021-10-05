98af52cb8d394e2c3a0a82b1ee2aa14ed790d103
Namhyeok <dr_lunars@naver.com>
2021-10-05 09:18:20 +0900

    practice

--- Level 2/N개의 최소공배수.py
+++ Level 2/N개의 최소공배수.py
@@ -1,30 +1,13 @@
-def solution(arr):
-    if len(arr) == 1:
-        return arr[0]
-    elif len(arr) == 2:
-        answer = lcm(arr[0],arr[1])
-        return answer
-    else:
-        answer = lcm(arr[0],arr[1])
-        for i in range(2,len(arr)):
-            answer = lcm(answer,arr[i])
-        return answer
+def gcd(n,m):
+    n,m = sorted([n,m])
+    while n:
+        n,m = m%n,n
+    return m
 
-def lcm(n, m):
-    answer = []
-    M = m
-    N = n
-    while True:
-        if M>=N:
-            M = M%N
-            if M == 0:
-                answer.append(N)
-                answer.append(N*(m//N)*(n//N))
-                break
-        else:
-            N = N%M
-            if N == 0:
-                answer.append(M)
-                answer.append(M*(m//M)*(n//M))
-                break
-    return answer[1]
+def lcm(n,m):
+    return n*m//gcd(n,m)
+
+def solution(arr):
+    while len(arr) >= 2:
+        arr.append(lcm(arr.pop(),arr.pop()))
+    return arr[0]
