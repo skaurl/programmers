7e2046dc0ae8b8d1a632be694d001204c8a47d7b
skaurl <dr_lunars@naver.com>
2021-11-05 17:49:36 +0900

    practice

--- Level 1/로또의 최고 순위와 최저 순위.py
+++ Level 1/로또의 최고 순위와 최저 순위.py
@@ -1,11 +1,3 @@
 def solution(lottos, win_nums):
-    answer = [0,0]
-    rank = [6,6,5,4,3,2,1]
-    cnt = 0
-    tmp = lottos.count(0)
-    for i in lottos:
-        if i in win_nums:
-            cnt += 1
-    answer[0] = rank[cnt+tmp]
-    answer[1] = rank[cnt]
-    return answer
+    cnt = len(set(lottos) & set(win_nums))
+    return [7 - max(cnt + lottos.count(0), 1), 7 - max(cnt, 1)]
