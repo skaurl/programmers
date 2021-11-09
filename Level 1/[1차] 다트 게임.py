56c0a7ae9b3a637fa6040cf57f63a528daa23016
skaurl <dr_lunars@naver.com>
2021-11-09 21:12:46 +0900

    practice

--- Level 1/[1차] 다트 게임.py
+++ Level 1/[1차] 다트 게임.py
@@ -1,22 +1,19 @@
 def solution(dartResult):
-    score = []
-    n = ''
-    for i in dartResult:
-        if i.isnumeric():
-            n += i
-        elif i == 'S':
-            score.append(int(n) ** 1)
-            n = ''
-        elif i == 'D':
-            score.append(int(n) ** 2)
-            n = ''
-        elif i == 'T':
-            score.append(int(n) ** 3)
-            n = ''
-        elif i == '*':
-            if len(score) > 1:
-                score[-2] *= 2
-            score[-1] *= 2
-        elif i == '#':
-            score[-1] *= -1
-    return sum(score)
+    answer = []
+    dartResult = dartResult.replace('10','k')
+    point = ['10' if i == 'k' else i for i in dartResult]
+    i = -1
+    sdt = ['S', 'D', 'T']
+    for j in point:
+        if j in sdt :
+            answer[i] = answer[i] ** (sdt.index(j)+1)
+        elif j == '*':
+            answer[i] = answer[i] * 2
+            if i != 0 :
+                answer[i - 1] = answer[i - 1] * 2
+        elif j == '#':
+            answer[i] = answer[i] * (-1)
+        else:
+            answer.append(int(j))
+            i += 1
+    return sum(answer)
