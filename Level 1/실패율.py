7340d3ff4c87677be99fdaf071e253fd6b999dc6
skaurl <dr_lunars@naver.com>
2021-11-10 21:17:14 +0900

    practice

--- Level 1/실패율.py
+++ Level 1/실패율.py
@@ -1,11 +1,11 @@
 def solution(N, stages):
-    result = {}
-    denominator = len(stages)
-    for stage in range(1, N+1):
-        if denominator != 0:
-            count = stages.count(stage)
-            result[stage] = count / denominator
-            denominator -= count
+    answer = {}
+    total = len(stages)
+    for i in range(1,N+1):
+        if total != 0:
+            cnt = stages.count(i)
+            answer[i] = cnt / total
+            total -= cnt
         else:
-            result[stage] = 0
-    return sorted(result, key=lambda x : result[x], reverse=True)
+            answer[i] = 0
+    return sorted(answer, key=lambda x : answer[x], reverse=True)
