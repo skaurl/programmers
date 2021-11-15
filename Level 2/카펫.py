5ca2983faf5deefed2da4fd7794285aa03f9e40b
skaurl <dr_lunars@naver.com>
2021-11-15 16:40:53 +0900

    practice

--- Level 2/카펫.py
+++ Level 2/카펫.py
@@ -1,7 +1,7 @@
 def solution(brown, yellow):
-    s = brown + yellow
-    for i in range(s,2,-1):
-        if s % i == 0:
-            a = s // i
-            if yellow == (i-2)*(a-2):
-                return [i,a]
+    total = brown + yellow
+    for i in range(total, 2, -1):
+        if total % i == 0:
+            j = total // i
+            if yellow == (i - 2) * (j - 2):
+                return [i, j]
